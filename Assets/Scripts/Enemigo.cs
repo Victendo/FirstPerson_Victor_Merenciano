009af0a0b6bffcd5f6cbad50f8e97bcf645756d3
Victendo <victormm2003@gmail.com>
2024-11-13 21:17:42 +0100

    13 / 11

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemigo : MonoBehaviour
 {
+
+    private NavMeshAgent agent;
+    private FirstPerson player;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+
+        player = GameObject.FindObjectOfType<FirstPerson>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        agent.SetDestination(player.transform.position);
         
+        if(agent.remainingDistance <= agent.stoppingDistance)
+        {
+            agent.isStopped = true;
+        }
+        else
+        {
+            agent.isStopped = false;
+        }
     }
 }
