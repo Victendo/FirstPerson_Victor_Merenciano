0a4781b7236858e211e99047278a2828644af1d4
Victendo <victormm2003@gmail.com>
2024-11-27 15:18:24 +0100

    27 / 11

--- Assets/Scripts/FirstPerson.cs
+++ Assets/Scripts/FirstPerson.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class FirstPerson : MonoBehaviour
 {
-    [SerializeField] private float velocidadMovimiento;
+    [Header("Variables Movimiento")]
     CharacterController controller;
     private Camera cam;
+    
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private float nivelGravedad;
+    private Vector3 movimientoVertical; 
+    [SerializeField] private float alturaSalto;
+    
+
+    [Header("Variables DeteccionDeSuelo")]
+    [SerializeField] private Transform piesPlayer;
+    [SerializeField] private float radioDeteccion;
+    [SerializeField] private LayerMask queEsSuelo;
+
+    Vector2 input;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,13 +36,52 @@ public class FirstPerson : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
         Vector2 input = new Vector2(h, v).normalized;
+
+        
+        transform.eulerAngles = new Vector3(0, cam.transform.eulerAngles.y, 0);
+
+
         if (input.sqrMagnitude > 0)
         {
             float anguloRotacion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
             transform.eulerAngles = new Vector3(0, anguloRotacion, 0);
+
+
             Vector3 movimiento = Quaternion.Euler(0, anguloRotacion, 0) * Vector3.forward;
             controller.Move(movimiento * velocidadMovimiento * Time.deltaTime);
         }
 
+        Gravedad();
+        DeteccionSuelo();
+
+    }
+
+    private void Gravedad()
+    {
+        
+        movimientoVertical.y += nivelGravedad * Time.deltaTime;
+        controller.Move(movimientoVertical * Time.deltaTime);
+
+    }
+
+    private void DeteccionSuelo()
+    {
+        
+        Collider[] collsDetectados = Physics.OverlapSphere(piesPlayer.position, radioDeteccion, queEsSuelo);
+        
+        if (collsDetectados.Length > 0)
+        {
+            movimientoVertical.y = 0;
+            Salto();
+
+        }
+    }
+
+    private void Salto()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            movimientoVertical.y = Mathf.Sqrt(-2 * nivelGravedad * alturaSalto);
+        }
     }
 }
