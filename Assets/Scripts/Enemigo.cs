ca9933e31c9da35cb5ee66ee80097e9520484b0d
Victendo <victormm2003@gmail.com>
2024-12-25 19:53:17 +0100

    25/12 P1

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,10 +9,13 @@ public class Enemigo : MonoBehaviour
 
     private NavMeshAgent agent;
     private FirstPerson player;
+    private Animator anim;
+    private bool ventanaAbierta = false;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
 
         player = GameObject.FindObjectOfType<FirstPerson>();
     }
@@ -19,16 +23,40 @@ public class Enemigo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Perseguir();
 
+       
+
+    }
+
+   
+
+    private void Perseguir()
+    {
         agent.SetDestination(player.transform.position);
-        
-        if(agent.remainingDistance <= agent.stoppingDistance)
+
+        if (agent.remainingDistance <= agent.stoppingDistance)
         {
             agent.isStopped = true;
-        }
-        else
-        {
-            agent.isStopped = false;
+            anim.SetBool("attacking", true);
         }
     }
+
+    #region Eventos de animacion
+    private void FinAtaque()
+    {
+        agent.isStopped = false;
+        anim.SetBool("attacking", false);
+    }
+
+    private void AbrirVentanaAtaque()
+    {
+        ventanaAbierta = true;
+    }
+    private void CerrarVentanaAtaque()
+    {
+        ventanaAbierta = false;
+    }
+
+    #endregion
 }
