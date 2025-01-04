2099e4df4e0302dab99e66f5a43b459911b7f82b
Victendo <victormm2003@gmail.com>
2025-01-04 17:44:50 +0100

    04 / 01

---
+++ Assets/Scripts/SistemaInteracciones.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SistemaInteracciones : MonoBehaviour
+{
+
+    private Camera cam;
+    [SerializeField] private float distanciaInteraccion;
+
+    private Transform interactuableActual;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hit, distanciaInteraccion))
+        {
+            if(hit.transform.TryGetComponent(out CajaMunicion scriptCaja))
+            {
+                interactuableActual = hit.transform;
+                interactuableActual.GetComponent<Outline>().enabled = true;
+
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    scriptCaja.Abrir();
+                }
+            }
+        }
+
+        else if (interactuableActual != null)
+        {
+            interactuableActual.GetComponent<Outline>().enabled = false;
+            interactuableActual = null;
+        }
+
+    }
+}
