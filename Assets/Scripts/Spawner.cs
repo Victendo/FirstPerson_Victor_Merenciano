eb06d8037507f52dda8aa056f427423bb016ac79
Victendo <victormm2003@gmail.com>
2025-01-05 13:10:03 +0100

    05 / 01

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Transform[] puntosSpawn;
+    [SerializeField] private Enemigo enemigoPrefab;
+    void Start()
+    {
+        StartCoroutine(Spawnear());
+    }
+
+    private IEnumerator Spawnear()
+    {
+        while(true)
+        {
+            Instantiate(enemigoPrefab, puntosSpawn[Random.Range(0, puntosSpawn.Length)].position, Quaternion.identity);
+            yield return new WaitForSeconds(2);
+        }
+    }
+
+    
+}
